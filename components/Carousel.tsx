e425da32a6ca09814ca9a41bfab8b8cdf56cc2cc
Jorgeluis006 <cuentaparajugarenlan007@gmail.com>
2025-11-03 17:59:38 -0500

    Revert carousel to original single image

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,25 +1,13 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Carousel.css';
 
 const images = [
   '/images/lw-cosmetics-nueva.png',
-  '/images/318318607_1135578437145790_2604222484458761246_n.jpg',
-  '/images/hermosa-mujer-joven-con-maquillaje-profesional.jpg',
-  '/images/vista-lateral-del-cepillo-de-labios-de-mano.jpg',
 ];
 
 export default function Carousel() {
   const [current, setCurrent] = useState(0);
-
-  // Auto-play: cambiar imagen cada 5 segundos
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   const prevSlide = () => {
     setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1));
