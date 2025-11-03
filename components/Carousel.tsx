4ad9b67089a15869b2f95b3a44382451053524c4
Jorgeluis006 <cuentaparajugarenlan007@gmail.com>
2025-11-03 18:06:42 -0500

    Add carousel with 6 images from new folder, starting with lw-cosmetics-nueva

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,13 +1,27 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.css';
 
 const images = [
-  '/images/lw-cosmetics-nueva.png',
+  '/images/imagenes del carrusel/lw-cosmetics-nueva.png',
+  '/images/imagenes del carrusel/maquillaje_con_productos_tambien_puedes_hacerlas_de_este_tamaao_porfa_2400_x_800_pixels_proporcin_3_w71lpe0gy3mm5sn24gyq_1.png',
+  '/images/imagenes del carrusel/maquillaje_con_productos_tambien_puedes_hacerlas_de_este_tamaao_porfa_2400_x_800_pixels_proporcin_3_iu37mj0fi2amof5xrdjc_1.png',
+  '/images/imagenes del carrusel/maquillaje_con_productos_tambien_puedes_hacerlas_de_este_tamaao_porfa_2400_x_800_pixels_proporcin_3_fcjmtvqjkkziqplps4f4_2.png',
+  '/images/imagenes del carrusel/maquillaje_con_productos_tambien_puedes_hacerlas_de_este_tamaao_porfa_2400_x_800_pixels_proporcin_3_o6h9o36phh98tjtebqus_2.png',
+  '/images/imagenes del carrusel/maquillaje_con_productos_tambien_puedes_hacerlas_de_este_tamaao_porfa_2400_x_800_pixels_proporcin_3_iyjcu9gejromfl192xnf_3.png',
 ];
 
 export default function Carousel() {
   const [current, setCurrent] = useState(0);
+
+  // Auto-play: cambiar imagen cada 5 segundos
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const prevSlide = () => {
     setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1));
